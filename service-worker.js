9adaf5a5f99cd19b0522878a09f118fb1f02a812
harizm837-dot <harizm837@gmail.com>
2026-02-06 00:46:45 +0800

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,33 @@
+const CACHE_NAME='erdene-nutag-cache-v1';
+const urlsToCache=[
+  './',
+  './index.html',
+  './manifest.json',
+  './service-worker.js'
+];
+
+// Install SW
+self.addEventListener('install', e=>{
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache=>cache.addAll(urlsToCache))
+  );
+});
+
+// Activate SW
+self.addEventListener('activate', e=>{
+  e.waitUntil(
+    caches.keys().then(keys=>{
+      return Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)));
+    })
+  );
+});
+
+// Fetch
+self.addEventListener('fetch', e=>{
+  e.respondWith(
+    caches.match(e.request).then(response=>{
+      return response || fetch(e.request);
+    })
+  );
+});
